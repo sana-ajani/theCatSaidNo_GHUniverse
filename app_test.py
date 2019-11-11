707925ece9c1a5f52779b5fa1e2086751a49f712
Sana Ajani <sanaajani@msn.com>
2019-11-11 14:10:08 -0800

    remove pytest

--- app_test.py
+++
@@ -1,8 +0,0 @@
-from app import app
-import pytest
-
-def test_home():
-    test_client = app.test_client()
-    response = test_client.get('/')
-    print(response.data)
-    assert b'error' not in response.data
